19074fa59d5e40c1fb56958377deae48a2714a73
Jinwei <jinweixing1006@gmail.com>
2020-07-18 18:13:56 -0700

    keep on converting..

--- deep_rl/agent/A2C_agent.py
+++ deep_rl/agent/A2C_agent.py
@@ -13,59 +13,46 @@ from .a2c_policy import A2CPolicy
 
 class A2CAgent(BaseAgent):
     def __init__(self, config, network, env):
-        BaseAgent.__init__(self, config)
-        self.config = config
-        self.task = env
-        self.storage = Rollout(config.rollout_length)
+        BaseAgent.__init__(self, config, env)
         config.optimizer = 'RMSprop'
         config.lr = 1e-4
         self.policy = A2CPolicy(network, config)
-        # self.network = network
-        # self.optimizer = config.optimizer_fn(self.network.parameters())
-        self.total_steps = 0
-        self.states = tensor(self.task.reset())
-        self.keys = ['a', 'ret', 'adv', 's']
-        self.rollout_filled = 0
+        self.storage = Rollout(config.rollout_length)
         self.rollout_length = config.rollout_length
-        self.num_workers = config.num_workers
-        self.discount = config.discount
+        self.sample_keys = ['s', 'a', 'ret', 'adv']
+        self.rollout_filled = 0
 
     def step(self):
-        # config = self.config
-        # storage = Rollout(self.rollout_length)
         with torch.no_grad():
-            prediction = self.policy.compute_actions(self.states)
-            # print(prediction[0].shape)
-        next_states, rewards, terminals, info = self.task.step(to_np(prediction[0]))
+            action, log_prob, v, ent = self.policy.compute_actions(self.state)
+        next_state, rwd, done, info = self.env.step(action.cpu().numpy())
         self.record_online_return(info)
-            # rewards = config.reward_normalizer(rewards)
-            # storage.add(prediction)
-        self.storage.add({'r': tensor(rewards),
-                         'm': tensor(1 - terminals),
-                         'a': prediction[0],
-                         'v': prediction[2],
-                         's': self.states})
 
-        self.total_steps += self.num_workers
+        self.storage.add({'a': action,
+                          'v': v, 
+                          'r': tensor(rwd),
+                          'm': tensor(1-done),
+                          's': self.state})
+        self.state = tensor(next_state)
         self.rollout_filled += 1
-        self.states = tensor(next_states)
+        # self.total_steps += self.num_workers
 
         if self.rollout_filled == self.rollout_length:
             with torch.no_grad():
-                prediction = self.policy.compute_actions(self.states)
-            self.storage.compute_returns(prediction[2], self.discount)
-            self.storage.after_fill(self.keys)
+                _, _, v, _ = self.policy.compute_actions(self.state)
+            self.storage.compute_returns(v, self.discount)
+            self.storage.after_fill(self.sample_keys)
 
-            indices = list(range(self.rollout_length * self.num_workers))
-            batch = self.sample(self.storage, indices)
+            indices = list(range(self.rollout_length*self.num_workers))
+            batch = self.sample(indices)
             loss = self.policy.learn_on_batch(batch)
 
             self.rollout_filled = 0
             self.storage.reset()
 
 
-    def sample(self, storage, indices):
+    def sample(self, indices):
         batch = {}
-        for k in self.keys:
-            batch[k] = getattr(storage, k)[indices]
+        for k in self.sample_keys:
+            batch[k] = getattr(self.storage, k)[indices]
         return batch
